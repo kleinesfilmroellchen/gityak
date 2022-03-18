9f9682f6d99b04959a94cf3c447356d41e4c90dc
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-03-18 12:24:20 +0100

    Replace "Issues" and "Pull requests" by Yak equiv
    Issues -> Yak stack, Pull requests -> Yaks being shaved

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+const prText = "Yaks being shaved";
+const issueText = "Yak stack";
+
 window.addEventListener("load", () => {
   const taskTexts = document.querySelectorAll(
     '[data-target="tracked-issues-progress.label"]'
@@ -5,4 +8,11 @@ window.addEventListener("load", () => {
   for (let taskText of taskTexts) {
     taskText.innerHTML = taskText.innerHTML.replaceAll(/task/g, "yak");
   }
+
+  document.querySelector('a[href="/pulls"]').innerHTML = prText;
+  document.querySelector('a[href="/issues"]').innerHTML = issueText;
+
+  document.querySelector('span[data-content="Pull requests"]').innerHTML =
+    prText;
+  document.querySelector('span[data-content="Issues"]').innerHTML = issueText;
 });
