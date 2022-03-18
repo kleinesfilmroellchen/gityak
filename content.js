a4920e605f79a369bc1f6246c3daa6a3e4286742
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-03-18 12:41:57 +0100

    Use SerenityOS custom emojis if available
    This is done through UA sniffing, so not super clean but eh

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
-const prText = "Yaks being shaved";
-const issueText = "Yak stack";
+let prText = "Yaks being shaved";
+let issueText = "Yak stack";
+
+const serenityUAMatcher = /SerenityOS/gi;
 
 window.addEventListener("load", () => {
   const taskTexts = document.querySelectorAll(
@@ -7,6 +9,17 @@ window.addEventListener("load", () => {
   );
   for (let taskText of taskTexts) {
     taskText.innerHTML = taskText.innerHTML.replaceAll(/task/g, "yak");
+  }
+
+  if (serenityUAMatcher.test(navigator.userAgent)) {
+    console.log(
+      "Congratulations, you're running GitYak in SerenityOS! You must have shaved a lot of yaks today. Take a break maybe, and then inform kleines Filmröllchen that the emojis don't actually work."
+    );
+    // https://serenityos.net/~xexxa/10CD
+    // IMMINENTYAKSHAVE (U+10CD01)
+    prText = "\u{10CD01}";
+    // YAKSTACK (U+10CD90)
+    issueText = "\u{10CD90}";
   }
 
   document.querySelector('a[href="/pulls"]').innerHTML = prText;
